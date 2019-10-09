e5ec0626179a471007a78afc1105833ac45484ee
zSlReN <hiraki.uk@gmail.com>
2019-10-10 00:56:42 +0100

    fixed .env bugs

--- main.py
+++ main.py
@@ -14,6 +14,20 @@ from logger import Logger
 target_date = datetime.datetime(2019, 10, 11, 15)
 
 
+def twitter_setup():
+	# path to environment variables
+	env_path = '.env'
+	load_dotenv(dotenv_path=env_path)
+
+	# get data from .env
+	key = os.getenv('KEY')
+	key_secret = os.getenv('KEY_SECRET')	
+	token = os.getenv('TOKEN')
+	token_secret = os.getenv('TOKEN_SECRET')	
+
+	return key, key_secret, token, token_secret
+
+
 class Twittermanager:
 	def __init__(self, key, secret_key, token, token_secret, logger=None):
 		self.twitter_api = twitter.Api(
@@ -48,11 +62,8 @@ class Twittermanager:
 
 
 if __name__ == '__main__':
-	key = os.getenv('KEY')
-	secret_key = os.getenv('KEY_SECRET')
-	token = os.getenv('TOKEN')
-	secret_token = os.getenv('TOKEN_SECRET')
-
+	key, secret_key, token, secret_token = twitter_setup()
+	
 	tc = Twittermanager(
 		key,
 		secret_key,
